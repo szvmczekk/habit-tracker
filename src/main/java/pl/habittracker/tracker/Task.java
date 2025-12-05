79160290bd764e9b9dac5942583b68a1e831c045
szvmczek <szymon.lebensztejn.essa@gmail.com>
2025-12-05 17:02:39 +0100

    added Task Entity

---
+++ src/main/java/pl/habittracker/tracker/Task.java
@@ -0,0 +1,69 @@
+package pl.habittracker.tracker;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private boolean done;
+    private LocalDateTime creationDate;
+
+    public Task(String name, String description, boolean done) {
+        this.name = name;
+        this.description = description;
+        this.done = done;
+        this.creationDate = LocalDateTime.now();
+    }
+
+    public Task() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+}
