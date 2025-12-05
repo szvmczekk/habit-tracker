d75d33dfc90acf88b30cae7eebaa5e63070fb3b2
szvmczek <szymon.lebensztejn.essa@gmail.com>
2025-12-05 19:50:07 +0100

    chore: new profiles for dev and prod and basic liquibase implementations
    dev:
     runs at H2 database, implements test data
    prod:
     runs at MySql database
    
    - updated 0001 changelog so it creates column data as BIT
    - added testdata.sql
    - added application-dev.yaml and application-prod.yaml
    - modified application.yaml so it has properties that are acceptable for both profiles

---
+++ src/main/resources/db/changelog/testdata/testdata.sql
@@ -0,0 +1,7 @@
+INSERT INTO TASK(name,description,done,creation_date)
+VALUES
+        ('task1','desc1',false,now()),
+        ('task2','desc2',false,now()),
+        ('task3','desc3',false,now()),
+        ('task4','desc4',false,now()),
+        ('task5','desc5',false,now());
